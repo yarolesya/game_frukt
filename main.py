9e94dc57b1334c16d7c98d2627e636aeebbfbe80
yarolesya <yaro01072009@gmail.com>
2022-09-24 11:18:31 +0300

    удалил ненужную строчку

--- main.py
+++ main.py
@@ -164,7 +164,3 @@ while run:
   win.blit(frukt3, [600, sharik3])
   pygame.display.update()
 pygame.quit()
-
-
-
-# Successfully created project 'game_fruit_' on GitHub, but initial commit failed: Author identity unknown *** Please tell me who you are. Run git config --global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository. unable to auto-detect email address (got 'hexog@LAPTOP-74NC81B4.(none)')
